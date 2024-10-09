a48aacd09c3d38318492a45938362a6b1d8131e4
Dimasriooo <dd8308451@gmail.com>
2024-10-10 02:56:07 +0700

    JavaScipt Dasar ES6

---
+++ JavaScript/ES6/TaskES6.js
@@ -0,0 +1,13 @@
+const hitungTotal = (nama, ...nilai) => {
+    // Menggabungkan array nilai dengan array tambahan menggunakan spread operator
+    const nilaiTambahan = [...nilai, 3, 7];
+
+    // Menghitung total nilai dengan menggunakan metode reduce
+    const total = nilaiTambahan.reduce((acc, curr) => acc + curr, 0);
+
+    // Mencetak pesan ke console menggunakan template literals
+    console.log(`Nama: ${nama}, Total Nilai: ${total}`);
+};
+
+// Contoh penggunaan fungsi
+hitungTotal('Dimas Rio', 10, 20, 30);
